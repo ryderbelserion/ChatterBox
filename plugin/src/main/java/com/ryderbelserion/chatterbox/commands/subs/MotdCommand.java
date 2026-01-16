aaeeba2bbab36f649761c1ea07b39daa63632a1f
Ryder Belserion <contact@ryderbelserion.com>
2026-01-16 12:49:07 -0500

    Add placeholders to motd command

--- plugin/src/main/java/com/ryderbelserion/chatterbox/commands/subs/MotdCommand.java
+++ plugin/src/main/java/com/ryderbelserion/chatterbox/commands/subs/MotdCommand.java
@@ -3,11 +3,19 @@ package com.ryderbelserion.chatterbox.commands.subs;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.command.system.basecommands.CommandBase;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.ryderbelserion.chatterbox.ChatterBox;
 import com.ryderbelserion.chatterbox.api.constants.Messages;
+import com.ryderbelserion.chatterbox.api.enums.Support;
 import com.ryderbelserion.chatterbox.messages.MessageRegistry;
 import com.ryderbelserion.chatterbox.users.UserManager;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.cacheddata.CachedMetaData;
+import net.luckperms.api.model.user.User;
 import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MotdCommand extends CommandBase {
 
@@ -27,10 +35,28 @@ public class MotdCommand extends CommandBase {
     protected void executeSync(@NotNull final CommandContext context) {
         final CommandSender sender = context.sender();
 
-        if (!context.isPlayer()) {
-            this.registry.getMessage(Messages.message_of_the_day).send(sender);
+        final Map<String, String> placeholders = new HashMap<>();
+
+        placeholders.put("{player}", sender.getDisplayName());
+
+        if (sender instanceof PlayerRef player) {
+            if (Support.luckperms.isEnabled()) {
+                final LuckPerms luckperms = LuckPermsProvider.get();
+
+                final User user = luckperms.getPlayerAdapter(PlayerRef.class).getUser(player);
+
+                final CachedMetaData data = user.getCachedData().getMetaData();
+
+                final String prefix = data.getPrefix();
+                final String suffix = data.getSuffix();
+
+                placeholders.put("{prefix}", prefix == null ? "N/A" : prefix);
+                placeholders.put("{suffix}", suffix == null ? "N/A" : suffix);
+            }
+
+            this.userManager.getUser(player.getUuid()).ifPresentOrElse(user -> user.sendMessage(Messages.message_of_the_day), () -> this.registry.getMessage(Messages.message_of_the_day).send(sender));
         } else {
-            this.userManager.getUser(sender.getUuid()).ifPresentOrElse(user -> user.sendMessage(Messages.message_of_the_day), () -> this.registry.getMessage(Messages.message_of_the_day).send(sender));
+            this.registry.getMessage(Messages.message_of_the_day).send(sender, placeholders);
         }
 
         this.instance.reload();
