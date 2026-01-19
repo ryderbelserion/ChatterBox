eb382784c4e37bcba0d2cec79cf272456c38490b
Ryder Belserion <contact@ryderbelserion.com>
2026-01-19 16:20:35 -0500

    Add paper module

---
+++ minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/ChatterBox.java
@@ -0,0 +1,16 @@
+package com.ryderbelserion.chatterbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatterBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
