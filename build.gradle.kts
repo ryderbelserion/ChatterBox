da14a64bb665c608939ce625b81e19aa99b25c2b
Ryder Belserion <contact@ryderbelserion.com>
2026-01-23 01:36:21 -0500

    Add publish plugins to build conventions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,108 @@
-plugins {
-    `java-plugin`
+import utils.convertList
+import utils.updateMarkdown
 
-    `shared-plugin`
+plugins {
+    id("curseforge-plugin")
+    id("modrinth-plugin")
+    id("hangar-plugin")
+
+    `java-plugin`
+}
+
+val releaseType = rootProject.ext.get("release_type").toString()
+val color = rootProject.property("${releaseType.lowercase()}_color").toString()
+val isRelease = releaseType.equals("release", true)
+val git = feather.getGit()
+
+feather {
+    rootDirectory = rootProject.rootDir.toPath()
+
+    val data = git.getGithubCommit("${rootProject.property("repository_owner")}/${rootProject.name}")
+
+    val user = data.user
+
+    discord {
+        webhook {
+            group(rootProject.name.lowercase())
+            task("release-build")
+
+            if (System.getenv("BUILD_WEBHOOK") != null) {
+                post(System.getenv("BUILD_WEBHOOK"))
+            }
+
+            if (isRelease) {
+                username(user.getName())
+
+                avatar(user.avatar)
+            } else {
+                username(rootProject.property("author_name").toString())
+
+                avatar(rootProject.property("author_avatar").toString())
+            }
+
+            embeds {
+                embed {
+                    color(color)
+
+                    title("A new $releaseType version of ${rootProject.name} is ready!")
+
+                    //if (isRelease) {
+                    //    content("<@&${rootProject.property("discord_role_id").toString()}>")
+                    //}
+
+                    fields {
+                        field(
+                            "Version ${rootProject.version}",
+                            listOf(
+                                "*Click below to download!*",
+                                "<:modrinth:1115307870473420800> [Modrinth](https://modrinth.com/plugin/${rootProject.name.lowercase()}/version/${rootProject.version})",
+                                "<:hangar:1139326635313733652> [Hangar](https://hangar.papermc.io/${rootProject.property("repository_owner").toString().replace("-", "")}/${rootProject.name.lowercase()}/versions/${rootProject.version})"
+                            ).convertList()
+                        )
+
+                        field(
+                            ":bug: Report Bugs",
+                            "https://github.com/${rootProject.property("repository_owner")}/${rootProject.name}/issues"
+                        )
+
+                        field(
+                            ":hammer: Changelog",
+                            rootProject.ext.get("mc_changelog").toString().updateMarkdown()
+                        )
+                    }
+                }
+            }
+        }
+
+        webhook {
+            group(rootProject.name.lowercase())
+            task("failed-build")
+
+            if (System.getenv("BUILD_WEBHOOK") != null) {
+                post(System.getenv("BUILD_WEBHOOK"))
+            }
+
+            username(rootProject.property("mascot_name").toString())
+
+            avatar(rootProject.property("mascot_avatar").toString())
+
+            embeds {
+                embed {
+                    color(rootProject.property("failed_color").toString())
+
+                    title("Oh no! It failed!")
+
+                    thumbnail("https://raw.githubusercontent.com/ryderbelserion/Branding/refs/heads/main/booze.jpg")
+
+                    fields {
+                        field(
+                            "The build versioned ${rootProject.version} for project ${rootProject.name} failed.",
+                            "The developer is likely already aware, he is just getting drunk.",
+                            inline = true
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ buildSrc/src/main/kotlin/publish/curseforge-plugin.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("PackageDirectoryMismatch")
+
+import gradle.kotlin.dsl.accessors._ba5d290ab715e74c9896b4fa811d61b7.ext
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
+
+plugins {
+    id("net.darkhax.curseforgegradle")
+
+    id("shared-plugin")
+}
+
+val libs: VersionCatalog = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
+
+tasks.register("publishCurseForge", net.darkhax.curseforgegradle.TaskPublishCurseForge::class) {
+    apiToken = System.getenv("CURSEFORGE_TOKEN")
+
+    val projectId = rootProject.property("cf_project_id") as String?
+
+    var mainFile = upload(projectId, tasks.named<Jar>("jar").flatMap { it.archiveFile })
+
+    mainFile.addGameVersion(rootProject.property("cf_game_version") as String?)
+    mainFile.releaseType = rootProject.ext.get("release_type").toString().uppercaseFirstChar()
+    mainFile.displayName = "${rootProject.version as String}-${project.name}"
+    mainFile.changelog = rootProject.file("changelog.md").readText()
+
+}
