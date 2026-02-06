f7c9b68a5568f1db9d2e202d58595e497e9c88d3
Ryder Belserion <contact@ryderbelserion.com>
2026-02-06 13:06:23 -0500

    Move message of the day to player join event

--- minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/listeners/CacheListener.java
+++ minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/listeners/CacheListener.java
@@ -2,20 +2,13 @@ package com.ryderbelserion.chatterbox.paper.listeners;
 
 import com.ryderbelserion.chatterbox.paper.ChatterBox;
 import com.ryderbelserion.chatterbox.paper.api.ChatterBoxPaper;
-import com.ryderbelserion.chatterbox.api.constants.Messages;
 import com.ryderbelserion.chatterbox.paper.api.registry.PaperUserRegistry;
-import com.ryderbelserion.chatterbox.paper.api.registry.adapters.PaperSenderAdapter;
-import com.ryderbelserion.chatterbox.common.enums.FileKeys;
-import com.ryderbelserion.fusion.paper.builders.folia.FoliaScheduler;
-import com.ryderbelserion.fusion.paper.builders.folia.Scheduler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.spongepowered.configurate.CommentedConfigurationNode;
-import java.util.Map;
 
 public class CacheListener implements Listener {
 
@@ -25,32 +18,11 @@ public class CacheListener implements Listener {
 
     private final PaperUserRegistry userRegistry = this.platform.getUserRegistry();
 
-    private final PaperSenderAdapter adapter = this.platform.getSenderAdapter();
-
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerJoin(final PlayerJoinEvent event) {
         final Player player = event.getPlayer();
 
         this.userRegistry.addUser(player);
-
-        final Map<String, String> placeholders = Map.of(
-                "{player}", player.getName()
-        );
-
-        final CommentedConfigurationNode config = FileKeys.config.getYamlConfig();
-
-        if (config.node("root", "motd", "toggle").getBoolean(false)) {
-            final int delay = config.node("root", "motd", "delay").getInt(0);
-
-            if (delay > 0) {
-                new FoliaScheduler(this.plugin, Scheduler.global_scheduler) {
-                    @Override
-                    public void run() {
-                        adapter.sendMessage(player, Messages.message_of_the_day, placeholders);
-                    }
-                }.runDelayed(delay);
-            }
-        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
--- minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/listeners/TrafficListener.java
+++ minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/listeners/TrafficListener.java
@@ -1,17 +1,23 @@
 package com.ryderbelserion.chatterbox.paper.listeners;
 
+import com.ryderbelserion.chatterbox.api.constants.Messages;
 import com.ryderbelserion.chatterbox.common.api.discord.DiscordManager;
 import com.ryderbelserion.chatterbox.common.configs.discord.DiscordConfig;
 import com.ryderbelserion.chatterbox.common.configs.discord.features.alerts.PlayerAlertConfig;
+import com.ryderbelserion.chatterbox.common.enums.FileKeys;
 import com.ryderbelserion.chatterbox.common.managers.ConfigManager;
 import com.ryderbelserion.chatterbox.paper.ChatterBox;
 import com.ryderbelserion.chatterbox.paper.api.ChatterBoxPaper;
+import com.ryderbelserion.chatterbox.paper.api.registry.adapters.PaperSenderAdapter;
 import com.ryderbelserion.discord.api.enums.alerts.PlayerAlert;
+import com.ryderbelserion.fusion.paper.builders.folia.FoliaScheduler;
+import com.ryderbelserion.fusion.paper.builders.folia.Scheduler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.spongepowered.configurate.CommentedConfigurationNode;
 import java.util.Map;
 
 public class TrafficListener implements Listener {
@@ -24,18 +30,37 @@ public class TrafficListener implements Listener {
 
     private final ConfigManager configManager = this.platform.getConfigManager();
 
+    private final PaperSenderAdapter adapter = this.platform.getSenderAdapter();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         final DiscordConfig config = this.configManager.getDiscord();
 
-        if (config.isPlayerAlertsEnabled()) {
-            final Player player = event.getPlayer();
+        final Player player = event.getPlayer();
 
+        final Map<String, String> placeholders = Map.of(
+                "{player}", player.getName()
+        );
+
+        if (config.isPlayerAlertsEnabled()) {
             final PlayerAlertConfig alertConfig = config.getAlertConfig();
 
-            alertConfig.sendDiscord(player, this.discordManager.getGuild(), PlayerAlert.JOIN_ALERT, Map.of(
-                    "{player}", player.getName()
-            ));
+            alertConfig.sendDiscord(player, this.discordManager.getGuild(), PlayerAlert.JOIN_ALERT, placeholders);
+        }
+
+        final CommentedConfigurationNode configuration = FileKeys.config.getYamlConfig();
+
+        if (configuration.node("root", "motd", "toggle").getBoolean(false)) {
+            final int delay = configuration.node("root", "motd", "delay").getInt(0);
+
+            if (delay > 0) {
+                new FoliaScheduler(this.plugin, Scheduler.global_scheduler) {
+                    @Override
+                    public void run() {
+                        adapter.sendMessage(player, Messages.message_of_the_day, placeholders);
+                    }
+                }.runDelayed(delay);
+            }
         }
     }
 
