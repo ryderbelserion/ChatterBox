87785283aace5e90aace2d94e90a796dd733d9ad
Ryder Belserion <contact@ryderbelserion.com>
2026-02-08 23:34:50 -0500

    Copy paste is not so daijoubu

--- minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/commands/player/MotdCommand.java
+++ minecraft/paper/src/main/java/com/ryderbelserion/chatterbox/paper/commands/player/MotdCommand.java
@@ -3,21 +3,42 @@ package com.ryderbelserion.chatterbox.paper.commands.player;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.ryderbelserion.chatterbox.api.constants.Messages;
+import com.ryderbelserion.chatterbox.common.api.adapters.GroupAdapter;
 import com.ryderbelserion.chatterbox.paper.api.ChatterCommand;
 import com.ryderbelserion.fusion.kyori.permissions.PermissionContext;
 import com.ryderbelserion.fusion.paper.builders.commands.context.PaperCommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MotdCommand extends ChatterCommand {
 
     @Override
     public void run(@NotNull final PaperCommandContext context) {
-        this.platform.reload();
+        final CommandSender sender = context.getSender();
 
-        this.adapter.sendMessage(context.getSender(), Messages.reload_plugin);
+        final Map<String, String> placeholders = new HashMap<>();
+
+        placeholders.put("{player}", sender.getName());
+
+        if (sender instanceof Player player) {
+            this.userRegistry.getUser(player.getUniqueId()).ifPresent(user -> {
+                final GroupAdapter adapter = user.getGroupAdapter();
+
+                final Map<String, String> map = adapter.getPlaceholders();
+
+                if (!map.isEmpty()) {
+                    placeholders.putAll(map);
+                }
+            });
+        }
+
+        this.adapter.sendMessage(sender, Messages.message_of_the_day, placeholders);
     }
 
     @Override
